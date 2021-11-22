6a0097497025fcea52c07d577304f50845895ab3
felipesal <prof.felipe.salles@gmail.com>
2021-11-22 00:15:09 -0300

    hr-gateway-zuul created

---
+++ hr-gateway-zuul/src/test/java/com/felipesalles/hrgatewayzuul/HrGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesalles.hrgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
